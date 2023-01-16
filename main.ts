57ea6c6a4850469156d0ea416e886a98b4987da6
elopeguu <122781317+elopeguu@users.noreply.github.com>
2023-01-16 11:04:12 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    jugador_arriba.change(LedSpriteProperty.Y, -1)
+    jugador_abajo.change(LedSpriteProperty.Y, -1)
+    basic.pause(1000)
+    jugador_arriba.change(LedSpriteProperty.Y, 1)
+    jugador_abajo.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    jugador_arriba.change(LedSpriteProperty.X, 1)
+    jugador_abajo.change(LedSpriteProperty.Y, 1)
+    basic.pause(1000)
+    jugador_arriba.change(LedSpriteProperty.X, 1)
+    jugador_arriba.change(LedSpriteProperty.Y, -1)
+})
+let jugador_abajo: game.LedSprite = null
+let jugador_arriba: game.LedSprite = null
+jugador_arriba = game.createSprite(1, 3)
+jugador_abajo = game.createSprite(1, 4)
+let obstaculo = game.createSprite(4, 4)
 basic.forever(function () {
-	
+    obstaculo.change(LedSpriteProperty.X, -1)
+    basic.pause(1000)
+    if (obstaculo.get(LedSpriteProperty.X) == 0) {
+        obstaculo.set(LedSpriteProperty.X, 4)
+        obstaculo.set(LedSpriteProperty.Y, randint(3, 4))
+    }
+    if (obstaculo.isTouching(jugador_abajo)) {
+        game.gameOver()
+    }
+    if (obstaculo.isTouching(jugador_arriba)) {
+        game.gameOver()
+    }
 })
